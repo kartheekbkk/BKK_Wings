c72943232f9a668801b7ac4a7462e3f18557dc5a
kartheekbkk <85800733+kartheekbkk@users.noreply.github.com>
2023-08-09 11:03:09 +0200

    Update FlightSearchPage.java

--- src/test/java/com/artoftesting/pages/FlightSearchPage.java
+++ src/test/java/com/artoftesting/pages/FlightSearchPage.java
@@ -1,9 +1,7 @@
 package com.artoftesting.pages;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
